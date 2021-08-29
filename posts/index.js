d93165ae80adb3edd88c9e5c5f143fb7d30a9e48
Prasundas99 <prasundas115@gmail.com>
2021-08-29 14:38:39 +0530

    emmitted event

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { randomBytes } from 'crypto';
 import cors from 'cors';
+import axios from 'axios'
 
 const app = express();
 app.use(express.urlencoded({extended: true})); 
@@ -10,11 +11,11 @@ app.use(cors())
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
+app.get("/posts",(req, res) => {
 res.send(posts)
 })
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async(req, res) => {
     const id = randomBytes(4).toString('hex');
     const {title } = req.body;
 
@@ -22,6 +23,12 @@ app.post("/posts", (req, res) => {
         id, title
     };
 
+await axios.post(`http://localhost:4005/events`, {
+     type: 'PostCreated',
+     data: {
+        id , title 
+     }
+})
     res.status(201).send(posts[id]);
 }); 
 
