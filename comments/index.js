591f12f42570ea1772d2fe41cc1ec5f545381788
Prasundas99 <prasundas115@gmail.com>
2021-08-29 13:40:42 +0530

    Basic implementation of event bus

--- comments/index.js
+++ comments/index.js
@@ -3,9 +3,8 @@ import { randomBytes } from "crypto";
 import cors from 'cors';
 
 const app = express();
+app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 
 const commentsByPostId = {};
